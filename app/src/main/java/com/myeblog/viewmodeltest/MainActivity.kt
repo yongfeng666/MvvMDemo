b2f790ed7fbbecd5b79483ca0a7a8cb486943f68
caoyongfeng <yongfeng1209@163.com>
2021-03-05 19:04:26 +0800

    依赖

--- app/src/main/java/com/myeblog/viewmodeltest/MainActivity.kt
+++ app/src/main/java/com/myeblog/viewmodeltest/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.myeblog.viewmodeltest.databinding.ActivityMainBinding
 import com.myeblog.viewmodeltest.viewModel.MainModel
@@ -12,13 +13,12 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var inflate: ActivityMainBinding
 
-    private lateinit var viewModel:MainModel
+    private  val viewModel:MainModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         inflate = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(inflate.root)
-        viewModel = ViewModelProvider(this).get(MainModel::class.java)
 
         inflate.button.setOnClickListener {
             viewModel.count++
